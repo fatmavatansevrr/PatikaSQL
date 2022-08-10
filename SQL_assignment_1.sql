8b51ce043c83a8ab5e083a80adb1df527d206d42
fatmavatansevrr <69508653+fatmavatansevrr@users.noreply.github.com>
2022-08-10 12:10:01 +0300

    Update SQL_assignment_1.sql

--- SQL_assignment_1.sql
+++ SQL_assignment_1.sql
@@ -3,19 +3,19 @@
 --1st Answer;
 SELECT title, description FROM film;
 
---2st Answer;
+--2nd Answer;
 SELECT * FROM film
 WHERE length > 60 AND length < 75;
 
---3st Answer;
+--3rd Answer;
 SELECT * FROM film
 WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
 
---4st Answer;
+--4th Answer;
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
---5st Answer;
+--5th Answer;
 SELECT * FROM film
 WHERE length < 50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99);
 
