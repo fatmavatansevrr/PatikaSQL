53894f28c4484d1ca90e43f349eed0eb437cc093
fatmavatansevrr <69508653+fatmavatansevrr@users.noreply.github.com>
2022-08-10 12:10:18 +0300

    Update SQL_assignment_2.sql

--- SQL_assignment_2.sql
+++ SQL_assignment_2.sql
@@ -1,14 +1,14 @@
 --HOMEWORK_2
 
---1.st ans;
+--1st ans;
 SELECT * FROM film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
---2.st ans;
+--2nd ans;
 SELECT first_name, last_name FROM actor
 WHERE first_name IN ('Penelope', 'Nick' , 'Ed');
 
---3.st ans;
+--3rd ans;
 SELECT * FROM film
 WHERE (rental_rate IN (0.99, 2.99, 4.99)) AND  (replacement_cost IN (12.99, 15.99, 28.99));
 
